4c53e7b03cfa3956913f8de9dd59bc609f68c1af
zsdlove <34211176+zsdlove@users.noreply.github.com>
2019-04-01 00:31:48 +0800

    代码太挫，稍微修复了下
    代码太挫，稍微修复了下

--- AndroidCodeCheck.py
+++ AndroidCodeCheck.py
@@ -8,8 +8,155 @@ import getopt
 sys.path.append('plugin')
 from WebviewHideAPI_Check import WebviewHideAPI_Check
 #resultinfo={"xss":[item1,item2]},{}}  item={"path":"path","line":"line","linecode":"linecode"}
-resultinfo={}
-apkname=""
+#resultinfo={}
+#apkname=""
+
+class apkvulcheck:
+	def __init__(self):
+		self.resultinfo={}
+	#
+	#获取manifest.xml文件
+	#			
+	def getAndroidManifest(self,apkname):
+		apkfilelist=getApkFilePath()
+		print('py文件目录'+os.getcwd())
+		for apkfilepath in apkfilelist:
+			try:
+				zipfiles=zipfile.ZipFile(apkfilepath)
+				a=zipfiles.read('AndroidManifest.xml')
+				if a !='':
+					#apkname=apkfilepath.split('.')[0].split('\\')[-1]
+					print('apk文件名'+apkname)
+					apkdir=os.getcwd()+'\\workspace\\result\\'+apkname
+					print(apkdir)
+					if not os.path.exists(apkdir):
+						os.makedirs(apkdir)
+					dexfile=open('workspace/result/'+apkname+'/AndroidManifest.xml','wb')
+					dexfile.write(a)
+					print('获取AndroidManifest.xml文件成功')
+				else:
+					print('找不到AndroidManifest.xml文件！')
+			except:
+				continue
+		
+
+	def decompiledex(self,apkname):
+		path=os.getcwd()+'//workspace//result//'
+		self.getdexfile(apkname)#处理apk文件
+		cmd="java -jar lib/baksmali.jar -o workspace/result/"+apkname+" workspace/result/"+apkname+"/classes.dex"
+		os.system(cmd)
+		print('dex文件反编译成功')
+	def VulScanEngine(self,apkname):
+		apkresultpath=os.getcwd()+'//report//'+apkname
+		os.makedirs(apkresultpath)
+		resultfile=open(apkresultpath+"//_result.html",'w+')
+		resultfile2=open(apkresultpath+"//_result.txt",'w+')
+		features=getFeatureFromXml()
+		print("开始进dex反编译")
+		self.decompiledex(apkname)
+		print("开始进行AndroidManifest.xml反编译")
+		self.decompile_AndroidManifest(apkname)
+		print("开始进行安卓漏洞静态扫描")
+		path='./workspace/result/'+apkname
+		for root,dirs,files,in os.walk(path):
+				for file in files:
+					if os.path.splitext(file)[1] == '.smali':
+						refs={}
+						#print(os.path.join(root,file))
+						filepath=os.path.join(root,file)
+						className=os.path.splitext(file)[0]
+						print("开始扫描类文件："+filepath)
+						f=open(filepath,'rb')
+						lines=f.readlines()
+						lineslen=len(lines)
+						className=os.path.splitext(file)[0]
+						try:
+							while lineslen>0:
+									lineslen=lineslen-1
+									linecode=lines[lineslen]
+									linecode=str(linecode,encoding="utf-8")	
+									for vulname in features.keys():
+										if vulname not in self.resultinfo.keys():
+											self.resultinfo[vulname]=[]
+										for feature in features[vulname]['item']:
+											m=re.match(r'.*'+feature+'.*',linecode)
+											if m:
+													print("[+]找到疑似"+vulname+"漏洞点，地址是："+filepath)
+													#if vulCheckEngine(vulname,lines):
+													vulinfo={}
+													vulinfo["path"]=filepath
+													vulinfo["linecode"]=linecode
+													vulinfo["line"]=str(lineslen)
+													self.resultinfo[vulname].append(vulinfo)
+													resultfile2.write("[+]checked:"+vulname+" 地址："+filepath+"行数："+str(lineslen)+"\n")
+											else:
+												pass
+										
+						except:
+							pass
+		resultfile.write("<h2>白盒扫描漏洞报告</h2>")
+		for vul in self.resultinfo.keys():
+			resultfile.write("<div id='menu'><h3><a href=#"+vul+">"+vul+"漏洞</a></h3>")
+		for vul in self.resultinfo.keys():
+			resultfile.write("<div id="+vul+"><h3>"+vul+"漏洞</h3><a href='#menu'><h3>  返回漏洞目录</h3><a>")
+			count=0
+			for vulitem in self.resultinfo[vul]:
+				count=count+1
+				print("[+]找到疑似"+vul+"漏洞点！")
+				print("代码是："+vulitem["linecode"].strip())
+				print("行数："+vulitem["line"])
+				print("路径："+vulitem["path"])
+				resultfile.write("<h4>第"+str(count)+"处漏洞点</h4>")
+				resultfile.write("<p>代码是："+vulitem["linecode"].strip()+"</p>")
+				resultfile.write("<p>行数："+vulitem["line"]+"</p>")
+				resultfile.write("<p>路径："+vulitem["path"]+"</p>")
+				resultfile.write("</div>")
+		print(self.resultinfo)
+		input("按任意键结束");
+	#
+	#获得dex文件
+	#
+	def getdexfile(self,apkname):
+		apkfilelist=getApkFilePath()
+		print('py文件目录'+os.getcwd())
+		for apkfilepath in apkfilelist:
+			try:
+				zipfiles=zipfile.ZipFile(apkfilepath)
+				a=zipfiles.read('classes.dex')
+				if a !='':
+					#apkfileName=apkfilepath.split('.')[0].split('\\')[-1]
+					print('apk文件名'+apkname)
+					apkdir=os.getcwd()+'\\workspace\\result\\'+apkname
+					os.makedirs(apkdir)
+					dexfile=open('workspace/result/'+apkname+'/classes.dex','wb')
+					dexfile.write(a)
+					print('获取classes.dex文件成功')
+				else:
+					print('找不到classes.dex文件！')
+			except:
+				continue
+			
+	#
+	#对androidManifest.xml进行反编译，获得明文文件
+	#
+
+	def decompile_AndroidManifest(self,apkname):
+		path=os.getcwd()+'//workspace//result//'
+		#apkfileNames=getapkFileName()
+		self.getAndroidManifest(apkname)#获取apk中的AndroidManifest.xml文件
+		#for apkfileName in apkfileNames:
+
+		cmd="java -jar lib/AXMLPrinter2.jar workspace/result/"+apkname+"/AndroidManifest.xml > "+" workspace/result/"+apkname+"/AndroidManifest_resolved.xml"
+		print("打印cmd"+cmd)
+		os.system(cmd)
+		android_manifest_read("workspace/result/"+apkname+"/AndroidManifest_resolved.xml")
+		print('AndroidManifest.xml反编译成功！')
+			
+		
+	def run(self):
+		for apkfilename in getapkFileName():
+			apkname=apkfilename+str(random.randrange(1000,9999))
+			self.VulScanEngine(apkname)		
 #
 #从conf.xml文件中获取特征值
 #	
@@ -92,74 +239,7 @@ def getcmdargs():
 #apk漏洞静态扫描引擎入口
 #
 	
-def VulScanEngine():
-	apkfilename=apkname
-	apkresultpath=os.getcwd()+'//report//'+apkfilename
-	os.makedirs(apkresultpath)
-	resultfile=open(apkresultpath+"//_result.html",'w+')
-	resultfile2=open(apkresultpath+"//_result.txt",'w+')
-	features=getFeatureFromXml()
-	print("开始进dex反编译")
-	decompiledex()
-	print("开始进行AndroidManifest.xml反编译")
-	decompile_AndroidManifest()
-	print("开始进行安卓漏洞静态扫描")
-	path='./workspace/result/'+apkfilename
-	for root,dirs,files,in os.walk(path):
-			for file in files:
-				if os.path.splitext(file)[1] == '.smali':
-					refs={}
-					#print(os.path.join(root,file))
-					filepath=os.path.join(root,file)
-					className=os.path.splitext(file)[0]
-					print("开始扫描类文件："+filepath)
-					f=open(filepath,'rb')
-					lines=f.readlines()
-					lineslen=len(lines)
-					className=os.path.splitext(file)[0]
-					try:
-						while lineslen>0:
-								lineslen=lineslen-1
-								linecode=lines[lineslen]
-								linecode=str(linecode,encoding="utf-8")	
-								for vulname in features.keys():
-									if vulname not in resultinfo.keys():
-										resultinfo[vulname]=[]
-									for feature in features[vulname]['item']:
-										m=re.match(r'.*'+feature+'.*',linecode)
-										if m:
-												print("[+]找到疑似"+vulname+"漏洞点，地址是："+filepath)
-												#if vulCheckEngine(vulname,lines):
-												vulinfo={}
-												vulinfo["path"]=filepath
-												vulinfo["linecode"]=linecode
-												vulinfo["line"]=str(lineslen)
-												resultinfo[vulname].append(vulinfo)
-												resultfile2.write("[+]checked:"+vulname+" 地址："+filepath+"行数："+str(lineslen)+"\n")
-										else:
-											pass
-									
-					except:
-						pass
-	resultfile.write("<h2>白盒扫描漏洞报告</h2>")
-	for vul in resultinfo.keys():
-		resultfile.write("<div id='menu'><h3><a href=#"+vul+">"+vul+"漏洞</a></h3>")
-	for vul in resultinfo.keys():
-		resultfile.write("<div id="+vul+"><h3>"+vul+"漏洞</h3><a href='#menu'><h3>  返回漏洞目录</h3><a>")
-		count=0
-		for vulitem in resultinfo[vul]:
-			count=count+1
-			print("[+]找到疑似"+vul+"漏洞点！")
-			print("代码是："+vulitem["linecode"].strip())
-			print("行数："+vulitem["line"])
-			print("路径："+vulitem["path"])
-			resultfile.write("<h4>第"+str(count)+"处漏洞点</h4>")
-			resultfile.write("<p>代码是："+vulitem["linecode"].strip()+"</p>")
-			resultfile.write("<p>行数："+vulitem["line"]+"</p>")
-			resultfile.write("<p>路径："+vulitem["path"]+"</p>")
-			resultfile.write("</div>")
-	print(resultinfo)
-	input("按任意键结束");
+	
 	
 #
 #获得apk文件名
@@ -174,36 +254,8 @@ def getapkFileName():
 					filepath=filepath.split('.')[0].split('\\')[-1]
 					apkfilenamelist.append(filepath)
 	return apkfilenamelist
-	
-#
-#对classes.dex文件进行反编译，获得smali文件
-#
+		
 
-def decompiledex():
-	path=os.getcwd()+'//workspace//result//'
-	#apkfileNames=getapkFileName()
-	getdexfile()#处理apk文件
-	#for apkfileName in apkfileNames:
-	cmd="java -jar lib/baksmali.jar -o workspace/result/"+apkname+" workspace/result/"+apkname+"/classes.dex"
-	os.system(cmd)
-	print('dex文件反编译成功')
-	
-#
-#对androidManifest.xml进行反编译，获得明文文件
-#
-
-def decompile_AndroidManifest():
-	path=os.getcwd()+'//workspace//result//'
-	#apkfileNames=getapkFileName()
-	getAndroidManifest()#获取apk中的AndroidManifest.xml文件
-	#for apkfileName in apkfileNames:
-
-	cmd="java -jar lib/AXMLPrinter2.jar workspace/result/"+apkname+"/AndroidManifest.xml > "+" workspace/result/"+apkname+"/AndroidManifest_resolved.xml"
-	print("打印cmd"+cmd)
-	os.system(cmd)
-	android_manifest_read("workspace/result/"+apkname+"/AndroidManifest_resolved.xml")
-	print('AndroidManifest.xml反编译成功！')
-	
 #
 #从获得apk文件路径
 #
@@ -217,54 +269,6 @@ def getApkFilePath():
 					apkfilelist.append(filepath)
 	return apkfilelist
 	
-#
-#获得dex文件
-#
-
-def getdexfile():
-	apkfilelist=getApkFilePath()
-	print('py文件目录'+os.getcwd())
-	for apkfilepath in apkfilelist:
-		try:
-			zipfiles=zipfile.ZipFile(apkfilepath)
-			a=zipfiles.read('classes.dex')
-			if a !='':
-				#apkfileName=apkfilepath.split('.')[0].split('\\')[-1]
-				apkfileName=apkname
-				print('apk文件名'+apkfileName)
-				apkdir=os.getcwd()+'\\workspace\\result\\'+apkfileName
-				os.makedirs(apkdir)
-				dexfile=open('workspace/result/'+apkfileName+'/classes.dex','wb')
-				dexfile.write(a)
-				print('获取classes.dex文件成功')
-			else:
-				print('找不到classes.dex文件！')
-		except:
-			continue
-#
-#获取manifest.xml文件
-#			
-def getAndroidManifest():
-	apkfilelist=getApkFilePath()
-	print('py文件目录'+os.getcwd())
-	for apkfilepath in apkfilelist:
-		try:
-			zipfiles=zipfile.ZipFile(apkfilepath)
-			a=zipfiles.read('AndroidManifest.xml')
-			if a !='':
-				#apkname=apkfilepath.split('.')[0].split('\\')[-1]
-				print('apk文件名'+apkname)
-				apkdir=os.getcwd()+'\\workspace\\result\\'+apkname
-				print(apkdir)
-				if not os.path.exists(apkdir):
-					os.makedirs(apkdir)
-				dexfile=open('workspace/result/'+apkname+'/AndroidManifest.xml','wb')
-				dexfile.write(a)
-				print('获取AndroidManifest.xml文件成功')
-			else:
-				print('找不到AndroidManifest.xml文件！')
-		except:
-			continue
 
 #
 #manifest.xml解析结果保存
@@ -420,11 +424,5 @@ def getPermission(node):
 	return node.getAttribute('android:name')
 	
 if __name__ == '__main__':
-	getcmdargs()
-'''
-	decompile_AndroidManifest()
-	apkfilenames=getapkFileName()
-	for apkfilename in apkfilenames:
-		apkname=apkfilename+str(random.randrange(1000,9999))
-		VulScanEngine()
-'''
+	avc=apkvulcheck()
+	avc.run()
